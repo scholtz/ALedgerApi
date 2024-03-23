4b7b1659c1b73e537e16dda8e06c5f71be92454a
janicek-hub <72258535+janicek-hub@users.noreply.github.com>
2024-03-23 20:22:24 +0100

    BFF Person, Person Unit Test (#4)
    * added POST for address
    
    * remove token
    
    * address PATCH
    
    * Address DELETE, GET{id}, GET
    
    * BFF Person, Person Unit Test
    
    ---------
    
    Co-authored-by: Ludovit Scholtz <ludko2@gmail.com>

---
+++ ALedgerBFFApi/Model/NewPerson.cs
@@ -0,0 +1,16 @@
+﻿namespace ALedgerBFFApi.Model
+{
+    public class NewPerson
+    {
+		public string BusinessName { get; set; }
+		public string? CompanyId { get; set; }
+		public string? CompanyTaxId { get; set; }
+		public string? CompanyVATId { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string Email { get; set; }
+		public string? Phone { get; set; }
+		public string AddressId { get; set; }
+		public string? SignatureUrl { get; set; }
+	}
+}
